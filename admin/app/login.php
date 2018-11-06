acb6d45b6c6fe63ed96744c4a7874171f23d21a2
Paulo Henrique Nunes <paulohcode@gmail.com>
2018-11-06 12:01:44 -0200

    Remoção do Admin

--- admin/app/login.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	session_cache_expire(5);
-	session_start();
-
-	if(!isset($_SESSION["sistema"]["id"])){
-		header("Location: index.php");
-	}
--- admin/dashboard.php
+++
@@ -1,9 +0,0 @@
-<h1>Dashboard</h1>
-<div class="row">
-	<div class="col text-center">
-		<a href="home.php?op=cadastro&pg=banner" class="thumbnail">
-			<i class="fa fa-user"></i>
-			<p>Cadastrar banner</p>
-		</a>
-	</div>
-</div>
